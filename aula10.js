2b2cfe5c24bab8fb870e47f03915e8878badc6dd
Gabriel-Henrique-Martins <80902575+Gabriel-Henrique-Martins@users.noreply.github.com>
2021-06-15 17:46:56 -0300

    aula 10 javascript

---
+++ aula10.js
@@ -0,0 +1,10 @@
+
+let main = document.querySelector('main')
+
+let header = document.querySelector('header') 
+
+header.innerHTML = "<h1 id='titulo'>Aula10</h1>"
+
+main.innerHTML = "<div><h2 class='meuh2'>ola</h2><p class='paragrafo'>bem vindo ao site criado no javascript</p></div><div><h2 class='meuh2'>ola</h2><p class='paragrafo'>site criado no javascript</p>"
+
+console.log(main)
